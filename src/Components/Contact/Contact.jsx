1cd761026aebd15128a716a88c2e76350f68e30c
Komal <ks989156@gmail.com>
2025-06-23 22:38:24 +0530

    Fixed typo

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
   return (
     <div className='flex flex-col p-4 h-[500px] items-center justify-center'>
       <motion.h1
-        nimate={{scale:[1,1,1.2,1]}}
+        animate={{scale:[1,1,1.2,1]}}
         transition={{duration:1.5}}
         className='text-center text-4xl font-semibold p-2 tracking-wide'>Stay Informed. Build Better.
       </motion.h1>
@@ -15,7 +15,7 @@ function Contact() {
       <div className='py-6'>
         <form action="">
           <input className='p-2 lg:px-4 m-2 lg:w-96 bg-black/20 dark:bg-white/20 outline-none rounded-lg font-light' type="text" placeholder='Enter your e-mail' />
-          <motion.button whileHover={{scale:1.1}} transition={{duration: 1, ease:'easeInOut'}} className='p-2 lg:px-6 m-2 border-1 border-cyan-400/80  dark:border-cyan-200/50 rounded-lg font-medium'>Subscribe</motion.button>
+          <motion.button whileHover={{scale:1.1}} transition={{duration: 0.4, ease:'easeInOut'}} className='p-2 lg:px-6 m-2 border-1 border-cyan-400/80  dark:border-cyan-200/50 rounded-lg font-medium'>Subscribe</motion.button>
         </form>
       </div>
     </div>
